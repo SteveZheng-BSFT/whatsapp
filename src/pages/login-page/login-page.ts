ceed52c41660b5ef0328554319aa4200679496cc
Steve Zheng <steve.zheng@globallogic.com>
2017-05-01 13:19:41 -0400

    added verification page

--- src/pages/login-page/login-page.ts
+++ src/pages/login-page/login-page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Alert, AlertController, NavController } from 'ionic-angular';
 import { PhoneService } from '../../services/phone';
+import { VerificationPage } from '../verification-page/verification-page';
 
 @Component({
   selector: 'page-login-page',
@@ -46,6 +47,11 @@ export class LoginPage {
   handleLogin(alert: Alert): void {
     alert.dismiss().then(() => {
       return this.phoneService.verify(this.phone);
+    })
+      .then(() => {
+      this.navCtrl.push(VerificationPage, {
+        phone: this.phone
+      });
     })
       .catch((e) => {
         this.handleError(e);
--- src/pages/verification-page/verification-page.ts
+++ src/pages/verification-page/verification-page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { AlertController, NavController, NavParams } from 'ionic-angular';
+import { PhoneService } from '../../services/phone';
 
 @Component({
   selector: 'page-verification-page',
@@ -7,11 +8,43 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class VerificationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private code: string = '';
+  private phone: string;
+
+  constructor(
+    private alertCtrl: AlertController,
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private phoneService: PhoneService
+  ) {}
+
+  ngOnInit() {
+    this.phone = this.navParams.get('phone');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VerificationPage');
+  onInputKeypress({keyCode}: KeyboardEvent): void {
+    if (keyCode === 13) {
+      this.verify();
+    }
+  }
+
+  verify(): void {
+    this.phoneService.login(this.phone, this.code)
+      .catch((e) => {
+        this.handleError(e);
+      });
+  }
+
+  handleError(e: Error): void {
+    console.error(e);
+
+    const alert = this.alertCtrl.create({
+      title: 'Oops!',
+      message: e.message,
+      buttons: ['OK']
+    });
+
+    alert.present();
   }
 
 }
